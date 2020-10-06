9ec08ef9aa3ee0db853f59943363a97538683f7a
irvingp <irvingpayan58@gmail.com>
2020-10-05 22:17:40 -0600

    agrego cambios

--- webpack.config.js
+++ webpack.config.js
@@ -21,7 +21,7 @@ module.exports = {
     devServer: {
         //contentBase: path.join( __dirname, '/public'),
         //compress: true,
-        port: 9000
+        port: 8080
       },
     plugins:
     [
